4f091f70ac699000d81e923f16ed7debb1442cfd
mosayeb <mb.masoumi1000@gmail.com>
2021-09-01 12:46:32 +0430

    some changes

--- app/src/main/java/com/example/viewpagertimer/MainActivity.java
+++ app/src/main/java/com/example/viewpagertimer/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.viewpagertimer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 
+import com.example.viewpagertimer.animation.CubeInDepthTransformation;
+import com.example.viewpagertimer.animation.FadeTransformation;
+import com.example.viewpagertimer.animation.PopTransformation;
 import com.example.viewpagertimer.viewpager.ViewPagerCustomDuration;
 
 
@@ -21,7 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
     CircleIndicator indicator;
     ViewPagerCustomDuration viewpager;
-    int position =0;
+
+
+    int sliderCurrentPosition = 0;
+    int sliderListSize = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
 
         viewpager = findViewById(R.id.viewpager);
         indicator = findViewById(R.id.indicator);
-
 
 
         List<String> list = new ArrayList<>();
@@ -43,62 +53,68 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), list);
         viewpager.setAdapter(adapter);
         indicator.setViewPager(viewpager);
-//      viewpager.setPageTransformer(true, new PopTransformation());
+        viewpager.setPageTransformer(true, new CubeInDepthTransformation());
 
 
-        int totalSize = list.size();
-
-
-
-        Timer t = new Timer();
-        t.scheduleAtFixedRate(new TimerTask() {
-                                  @Override
-                                  public void run() {
-
-                                      runOnUiThread(new Runnable() {
-                                          @Override
-                                          public void run() {
-
-                                              if(position==0){
-                                                  Log.i("TAG", "run: ");
-                                                  viewpager.setScrollDurationFactor(0.7);
-                                              }else{
-                                                  viewpager.setScrollDurationFactor(5);
-                                              }
-
-                                              viewpager.setCurrentItem(position);
-                                          }
-                                      });
-
-                                      position++;
-                                      if(position >= (totalSize)){
-                                          position=0;
-                                      }
-                                  }
-                              },
-//Set how long before to start calling the TimerTask (in milliseconds)
-                0,
-//Set the amount of time between each execution (in milliseconds)
-                3000);
-
+        sliderListSize = list.size();
+        sliderCurrentPosition = 0;
 
 
         viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int positionScrolled, float positionOffset, int positionOffsetPixels) {
-               position = positionScrolled;
+                sliderCurrentPosition = positionScrolled;
+                startSliderTimer();
             }
 
             @Override
             public void onPageSelected(int position) {
+//                sliderCurrentPosition = position;
+
 
             }
 
             @Override
             public void onPageScrollStateChanged(int state) {
 
+                Log.i("TAG", "onPageScrollStateChanged: ");
             }
         });
 
+    }
+
+    final Handler sliderHandler = new Handler(Looper.getMainLooper());
+
+    private void startSliderTimer() {
+
+        if (sliderHandler != null) {
+            // to stop counting
+            sliderHandler.removeCallbacksAndMessages(null);
+        }
+
+        if (sliderCurrentPosition < sliderListSize - 1) {
+            sliderCurrentPosition++;
+        } else {
+            sliderCurrentPosition = 0;
+        }
+
+        sliderHandler.postDelayed(() -> {
+            // here you check the value of getActivity() and break up if needed
+            if (this == null)
+                return;
+            this.runOnUiThread(() -> {
+
+                if (sliderCurrentPosition == 0) {
+                    Log.i("TAG", "run: ");
+                    viewpager.setScrollDurationFactor(0.0);
+                } else {
+                    viewpager.setScrollDurationFactor(8);
+                }
+
+
+                viewpager.setCurrentItem(sliderCurrentPosition);
+            });
+
+        }, 3000);
     }
 }
